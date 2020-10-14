86d52cbd7502e7183c7f09c5e3c91edad27c5ca8
paulmotz <pauljmotz@gmail.com>
2020-10-14 19:54:01 -0400

    Add dark mode toggle (that doesn't actually set dark mode)

--- src/components/DarkModeToggle.vue
+++ src/components/DarkModeToggle.vue
@@ -1,10 +1,8 @@
 <template lang='pug'>
 	.dark-mode-toggle
-		font-awesome-icon.fa-icon(
-			icon="sun")
-		toggle-checkbox
-		font-awesome-icon.fa-icon(
-			icon="moon")
+		font-awesome-icon.fa-icon(icon="sun")
+		toggle-checkbox(v-model="isDarkModeEnabled")
+		font-awesome-icon.fa-icon(icon="moon")
 </template>
 
 <script lang='ts'>
@@ -17,11 +15,21 @@ export default Vue.extend({
 	components : {
 		ToggleCheckbox,
 	},
+
+	data : function() {
+		return {
+			isDarkModeEnabled : false,
+		};
+	},
 });
 </script>
 
 <style scoped lang='stylus'>
 @import '../assets/variables.styl'
+.dark-mode-toggle
+	display: flex
+
 .fa-icon
 	color: $white
+	margin: 0 0.1875rem
 </style>
--- src/components/ToggleCheckbox.vue
+++ src/components/ToggleCheckbox.vue
@@ -1,5 +1,9 @@
 <template lang='pug'>
-	input.toggle-checkbox(type="checkbox")
+	.toggle(
+		v-bind:class="{ 'checked' : this.value }"
+		v-on:click="toggleValue")
+		input.toggle-checkbox(type="checkbox")
+		.switch
 </template>
 
 <script lang='ts'>
@@ -7,13 +11,49 @@ import Vue from 'vue';
 
 export default Vue.extend({
 	name : 'ToggleCheckbox',
+
+	props : {
+		value : {
+			type    : Boolean,
+			default : false,
+		},
+	},
+
+	methods : {
+		toggleValue(): void {
+			this.$emit('input', !this.value);
+		},
+	},
 });
 </script>
 
 <style scoped lang='stylus'>
 @import '../assets/variables.styl'
+.toggle
+	height: 1rem
+	width: 2rem
+	border-radius: 0.5rem
+	display: flex
+	cursor: pointer
+	align-items: center
+	background: $inactive-toggle-color
+
+.toggle.checked
+	background: $active-toggle-color
+
 .toggle-checkbox
-	height: 0;
-	width: 0;
-	visibility: hidden;
+	height: 0
+	width: 0
+	opacity: 0
+	position: absolute
+
+.switch
+	height: 0.875rem
+	width: 0.875rem
+	background: white
+	border-radius: 50%
+	transform: translateX(0)
+
+.checked .switch
+	transform: translateX(1rem)
 </style>
