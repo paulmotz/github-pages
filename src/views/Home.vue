43cec184c1be19e7b6d17ebd0dd506101a8c522c
paulmotz <pauljmotz@gmail.com>
2021-01-10 21:00:16 -0500

    Add basic intro text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,39 @@
-<template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome!... to Your Vue.js App" />
-  </div>
+<template lang='pug'>
+	.home-wrapper
+		p Welcome to my personal website!
+		p 
+			| This is my playground to build things with technologies that interest me. 
+			a(href="https://vuejs.org/") Vue.js
+			|  is the backbone (pun intended) of this site. This site uses several components of the Vue ecosystem including 
+			a(href="https://cli.vuejs.org/") Vue CLI
+			|  and 
+			a(href="https://router.vuejs.org/") Vue Router
+			| .
+			| Outside of Vue, this site also uses 
+			a(href="https://www.typescriptlang.org/") TypeScript
+			| , 
+			a(href="https://pugjs.org/api/getting-started.html") pug
+			| , 
+			a(href="https://stylus-lang.com/") stylus
+			|  and 
+			a(href="https://stylus-lang.com/") Jest
+			|  (❤️).
+		p I am using these technologies to build games, visualizations and track how I listen to music.
 </template>
 
 <script lang='ts'>
-// @ is an alias to /src
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default Vue.extend({
-  name       : 'Home',
-  components : {
-    HelloWorld,
-  },
+	name : 'Home',
 });
 </script>
+
+<style scoped lang='stylus'>
+.home-wrapper
+	margin: 5rem
+	font-size: 1.25rem
+
+	p
+		margin: 0 0 1rem 0
+</style>
